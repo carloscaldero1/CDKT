78397f9adcadaf3c10ba7ddd5cccb3d0609ae773
carloscaldero1 <70708115+carloscaldero1@users.noreply.github.com>
2020-12-17 11:10:54 -0500

    updated responsiveness of new version of profile.php

--- responsivePages/profile.php
+++ responsivePages/profile.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<style>
+body {
+  background-color: lightgrey;
+}
+</style>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -32,7 +36,7 @@ if(!isset($_SESSION['username'])){
 }
 
 if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SESSION['username'] == $_GET['username']) or (isset($_GET['recommendations'])) or (isset($_GET['Add']))){
-  
+	
 	echo '<div class="w3-quarter w3-black"> <a href="homepage.php">Homepage</a></div>';
 
 	echo '<div class="w3-quarter w3-black"> <a href="songDiscovery.php">Song Discovery</a></div>';
@@ -40,18 +44,41 @@ if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SES
 	echo '<div class="w3-quarter w3-black"><a href="songSearcher.php">Song Search</a></div>';
 
 	echo '<div class="w3-quarter w3-black"> <a href="logout.php?logout">Logout</a></div>';
- 
-  
-  
+
 	?>
   </div>
   <div class="w3-container w3-cyan">
   <h1><u>Welcome to Your Profile Page</u></h1></div>
+  
+  
+<div class="w3-row">
+  <div class="half">
+	<?php
+
+	$privacy = privacyGet($_SESSION['username']);
+
+        if($privacy == 1){
+                echo "<b>Your account is currently public.</b>";
+        }
+        elseif($privacy == 0){
+                echo "<b>Your account is currently private.</b>";
+        }
+
+	?>
+  </div>
+  <div class="half">
+	<form action="./profilePrivacy.php">
+	<input type="submit" name="public" value="Make Profile Public">
+	<input type="submit" name="private" value="Make Profile Private">
+	</form>	
+  </div>
+</div>
   <div class="w3-row">
-  <div class="w3-quarter w3-yellow ">
+  <div class="w3-quarter">
 	<h3><u>Your Playlist</h3></u>
- 
+  
   <?php
+
 	if(isset($_GET['Add'])){
         $songAdded = addSongToProfile($_SESSION['username'], $_GET['Add']);
 
@@ -66,34 +93,41 @@ if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SES
 	$outputArray = retrieveProfileSongs($_SESSION['username']);
 	?>
 	</div>
-  <div class="w3-threequarter w3-dark-grey ">
+  <div class="w3-threequarter w3-dark-grey">
 	<table border=2>
 	
 	<tr>
+	<th>Like?</th>
+	<th>Total Likes</th>
         <th>Song</th>
         <th>Album</th>
         <th>Artist</th>
         <th>Release Date</th>
         <th>Song Length</th>
-        <th>Popularity</th>
+        <!--<th>Popularity</th>
         <th>Mode/Scale</th>
-        <th>Key</th>
+        <th>Key</th>-->
         </tr> 
-
+	
 	<?php
 	if($outputArray != false){
 	for($i = 0; $i < count($outputArray); $i++){
 		?>
 
 		<tr>
+		<td><form action="./likeSong.php">
+                <input type="submit" name=<?php echo $outputArray[$i][1]; ?> value="Like/Unlike">
+                <input type="hidden" name="like" value=<?php echo $outputArray[$i][1];?>>
+                </form></td>
+		<td> <?php echo $outputArray[$i][10];?> </td>
 		<td> <?php echo $outputArray[$i][2];?> </td>
                 <td> <?php echo $outputArray[$i][3];?> </td>
                 <td> <?php echo $outputArray[$i][4];?> </td>
 		<td> <?php echo $outputArray[$i][5];?> </td>
 		<td> <?php echo milliConversion($outputArray[$i][6]);?> </td>
-		<td> <?php echo $outputArray[$i][7];?> </td>
+		<!--<td> <?php echo $outputArray[$i][7];?> </td>
 		<td> <?php echo convertMode($outputArray[$i][8]);?> </td>
-		<td> <?php echo convertKey($outputArray[$i][9]);?> </td>
+		<td> <?php echo convertKey($outputArray[$i][9]);?> </td>-->
 		</tr>
 
 		<?php
@@ -101,15 +135,16 @@ if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SES
 }
 	?></table>
   </div>
-  </div>
-  <div class="w3-row">
-    <div class="w3-quarter w3-red ">
+</div>
+<div class="w3-row">
+  <div class="w3-quarter">
 	<h3><u>Songs We Think You'd Like Based on Your Playlist</u></h3>
+
 	<?php
 	$recommendationArray = getRecommendedSongs($_SESSION['username']);
 	?>
   </div>
-  <div class="w3-threequarter w3-blue">
+  <div class="w3-threequarter w3-dark-grey">
 	<table border=2>
 
         <tr>
@@ -149,17 +184,18 @@ if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SES
 }
 	?>
 		
-	</table>
-    </div>
+	</table>	
   </div>
+</div>
 <div class="w3-row">
-  <div class="w3-green">
+  
 	<br>	
 	<form action="./profile.php">
 	<input type="submit" value="Get New Recommendations" name="recommendations"</input>
 	</form>
 
 	<h3><u>Comment Section</u></h3>
+		
 	<?php
 	$comments = getComments($_SESSION['username']);
 		
@@ -174,7 +210,6 @@ if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SES
 
 	?>
 
-
 	<form action='./personalComments.php'>
 		<input type='hidden' name='userProfile' value='<?php echo $_SESSION['username']?>'>
 		<input type='hidden' name='date' value='<?php echo date('Y-m-d  H:i:s')?>'>
@@ -182,32 +217,39 @@ if((isset($_SESSION['username']) and (!isset($_GET['profileSearch']))) or ($_SES
 		<br><button type='submit' name='commentSubmit'>Comment</button>
 	</form>
  
- </div>
+ 
 </div>
 <div class="w3-row w3-black">
 	<?php
 }
- 
+
 elseif(isset($_GET['profileSearch'])){
+	
 	$userProfile = $_GET['username'];
+	$privacyIs = privacyGet($userProfile);
 
-	echo '<div class="w3-quarter w3-black"><a href="homepage.php">Homepage</a></div>';
-	echo '<div class="w3-quarter w3-black"><a href="songDiscovery.php">Song Discovery</a></div>';
+	if($privacyIs==1){
+
+	echo '<div class="w3-quarter w3-black"> <a href="homepage.php">Homepage</a></div>';
+
+	echo '<div class="w3-quarter w3-black"> <a href="songDiscovery.php">Song Discovery</a></div>';
+
 	echo '<div class="w3-quarter w3-black"><a href="songSearcher.php">Song Search</a></div>';
-	echo '<div class="w3-quarter w3-black"><a href="logout.php?logout">Logout</a></div>';
-?>
-</div>
 
+	echo '<div class="w3-quarter w3-black"> <a href="logout.php?logout">Logout</a></div>';
+
+	?>
+</div>
 <div class="w3-container w3-cyan">
-<h1><u>Welcome to <?php echo $_GET['username'];?>'s Profile Page</u></h1></div>
+  <h1><u>Welcome to <?php echo $_GET['username'];?>'s Profile Page</u></h1></div>
 <div class="w3-row">
-    <div class="w3-quarter w3-pink">
+  <div class="w3-quarter">
 	<?php
-	echo "<h3><u>Below(right for mobile layout) is $userProfile's playlist.</u></h3>";
+	echo "<h3><u>Below is $userProfile's playlist.</u></h3>";
 	$outputArray = retrieveProfileSongs($_GET['username']);
         ?>
     </div>
-    <div class="w3-threequarter w3-orange">
+    <div class="w3-threequarter w3-dark-grey">
         <table border=2>
         <tr>
         <th>Song</th>
@@ -238,12 +280,12 @@ elseif(isset($_GET['profileSearch'])){
         }
 	
 	?></table>
-    </div>
+  </div>
 </div>
 <div class="w3-row">
-  <div class="w3-green">
+  
 	<h3><u>Comment Section</u></h3>
- 
+  
 	<?php
         $comments = getComments($_GET['username']);
 	
@@ -265,24 +307,27 @@ elseif(isset($_GET['profileSearch'])){
                 <textarea name='message' rows='5' cols='50'></textarea>
                 <br><button type='submit' name='commentSubmit'>Comment</button>
         </form>
-  </div>
+ 
 </div>
-
-<div class="w3-row">
+<div class="w3-row w3-black">
 <?php
+	}
+
+	elseif($privacyIs == 0){
+		echo "$userProfile has their profile set to private, sorry.";
+	}
 }
 
-echo '<div class="w3-quarter w3-black"><br><a href="homepage.php">Homepage</a></div>';
+  echo '<div class="w3-quarter w3-black"> <a href="homepage.php">Homepage</a></div>';
 
-echo '<div class="w3-quarter w3-black"><a href="songDiscovery.php">Song Discovery</a></div>';
+	echo '<div class="w3-quarter w3-black"> <a href="songDiscovery.php">Song Discovery</a></div>';
 
-echo '<div class="w3-quarter w3-black"><a href="songSearcher.php">Song Search</a></div>';
+	echo '<div class="w3-quarter w3-black"><a href="songSearcher.php">Song Search</a></div>';
 
-echo '<div class="w3-quarter w3-black"><a href="logout.php?logout">Logout</a></div>';
+	echo '<div class="w3-quarter w3-black"> <a href="logout.php?logout">Logout</a></div>';
 
 ?>
 </div>
 </center>
 </body>
-</html>
 </html>
